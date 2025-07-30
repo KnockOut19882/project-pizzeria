5010fbf11c06301e0d70978f70955d76d2db5d76
Mateusz <mpodolski1988@gmail.com>
2025-07-30 15:23:44 +0200

    Add initAccordion to js

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,7 @@ class Product {
       thisProduct.data = data;
       thisProduct.renderInMenu();
       console.log('new Product', thisProduct);
+      thisProduct.initAccordion();
     }
     renderInMenu() {
       const thisProduct = this;
@@ -91,6 +92,25 @@ class Product {
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       const menuContainer = document.querySelector(select.containerOf.menu);
       menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+    /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+      event.preventDefault();
+      /* find active product (product that has active class) */
+      const activeProduct = document.querySelector(select.containerOf.menu );
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+      if(activeProduct && activeProduct !== thisProduct.element){
+        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+      }
+      /* toggle active class on thisProduct.element */
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+    });
     }
   }
 
