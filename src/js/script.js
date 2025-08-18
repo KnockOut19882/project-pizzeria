b54bd60da526fe9b92d651e3ea6b41fca36d210e
Mateusz <mpodolski1988@gmail.com>
2025-08-18 20:50:53 +0200

    Change app.initData and app.initMenu

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@ const select = {
       defaultDeliveryFee: 20,
     },
   // CODE ADDED END
+    db: {
+    url: '//localhost:3131',
+    products: 'products',
+    orders: 'orders',
+},
   };
 
   const templates = {
@@ -280,12 +285,22 @@ const select = {
     initMenu: function() {
       const thisApp = this;
       for(let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
     initData: function() {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      const url = settings.db.url + '/' + settings.db.products;
+      fetch(url)
+        .then(function(rawResponse) {
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse) {
+          console.log('parsedResponse', parsedResponse);
+          thisApp.data.products = parsedResponse;
+          thisApp.initMenu();
+        });
     },
     init: function(){
       const thisApp = this;
@@ -295,7 +310,6 @@ const select = {
       console.log('settings:', settings);
       console.log('templates:', templates);
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
     initCart: function(){
