6768355b16f8c992e08329c5159dce96f30273fd
Mateusz <mpodolski1988@gmail.com>
2025-09-09 15:29:29 +0200

    Add file 'Booking' and function initBooking

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,14 @@
 import {settings, select, classNames, templates} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initBooking: function(){
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
   initPages: function(){
     const thisApp = this;
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
@@ -61,6 +67,7 @@ const app = {
       
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initBooking();
     },
     initCart: function(){
       const thisApp = this;
