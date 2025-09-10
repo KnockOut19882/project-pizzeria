53daf8c0daa7e2e2e707a80c1e1981002eefee39
Mateusz <mpodolski1988@gmail.com>
2025-09-10 10:18:11 +0200

    Add render and initWidgets in class Booking

--- dist/js/components/Booking.js
+++ dist/js/components/Booking.js
@@ -1,5 +1,28 @@
+import {templates, settings} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
 class Booking {
-  
+  constructor(bookingContainer){
+    const thisBooking = this;
+    thisBooking.render(bookingContainer);
+    thisBooking.initWidgets();
+
+  }
+
+  render(bookingContainer) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = bookingContainer;
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(settings.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(settings.booking.hoursAmount);
+  }
+  initWidgets() {
+    const thisBooking = this;
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  }
 }
 
 export default Booking;
